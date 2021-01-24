b4690211a2167c1864c0bee58a2a8fd83afca78b
Bartosz Kut <bartosz.kut.programista@gmail.com>
2021-01-24 21:18:21 +0100

    C:/Program Files/Git/feature - paddles movement

--- Unit1.h
+++ Unit1.h
@@ -16,7 +16,21 @@ __published:	// IDE-managed Components
         TShape *background;
         TImage *ball;
         TTimer *Timer_ball;
+        TImage *left_paddle;
+        TTimer *left_up;
+        TTimer *left_down;
+        TTimer *right_up;
+        TTimer *right_down;
+        TImage *right_paddle;
         void __fastcall Timer_ballTimer(TObject *Sender);
+        void __fastcall left_upTimer(TObject *Sender);
+        void __fastcall left_downTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall right_upTimer(TObject *Sender);
+        void __fastcall right_downTimer(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
